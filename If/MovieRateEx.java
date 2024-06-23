bd66c7f936465e416ef1dfa5b1410660ebbf6106
clov2r <118502976+clov2r@users.noreply.github.com>
2024-06-23 16:56:16 +0900

    Add files via upload

---
+++ If/MovieRateEx.java
@@ -0,0 +1,16 @@
+package cond;
+
+public class MovieRateEx {
+    public static void main(String[] args) {
+        double rating=7.0;
+        if (rating<=9.0) {
+            System.out.println("어바웃타임");
+        }
+        if (rating<=8.0) {
+            System.out.println("토이스토리");
+        }
+        if (rating<=7.0){
+            System.out.println("고질라");
+        }
+    }
+}
---
+++ Operator/OperationEx3.java
@@ -0,0 +1,11 @@
+package Operator;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score=70;
+        if (80<=score && score<=100)
+            System.out.println("true");
+        else
+            System.out.println("False");
+    }
+}
